89e09bc369018fd41d7b253522fb80f7e928e188
michelleschneider <xmichelle455@gmail.com>
2022-11-15 20:11:34 +0200

    Exercise 5 first version

--- src/Exercise5.java
+++ src/Exercise5.java
@@ -1,6 +1,34 @@
+import java.util.Scanner;
+
 public class Exercise5 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Enter a number: ");
+        int num = scanner.nextInt();
 
+        int sum;
+        int A0 = 0;
+        int A1 = 1;
+
+        if (num==0) {
+            System.out.println("The number is fibonacci");
+        }
+
+        else {
+            do  {
+                sum = A0 + A1;
+                A0 = A1;
+                A1 = sum;
+
+                if (sum == num) {
+                    System.out.println("The number is fibonacci");
+                }
+
+                else if (sum > num){
+                    System.out.println("The number is not fibonacci");
+                }
+            } while (num>sum);
+        }
     }
 }
