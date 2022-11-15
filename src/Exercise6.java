f580abf776290d2774bea52581b392ed7e63fd3b
Alik <alikharutyunyan00@gmail.com>
2022-11-15 20:28:38 +0200

    Exercise 6 completed

--- src/Exercise6.java
+++ src/Exercise6.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+
 public class Exercise6 {
     public static void main(String[] args) {
+        int digit;
+        int sum = 0;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int userNumber = scanner.nextInt();
 
+        while(userNumber > 0) {
+            digit = userNumber % 10;
+            userNumber = userNumber / 10;
+            sum+= digit;
+        }
+        System.out.println(sum);
     }
 }
