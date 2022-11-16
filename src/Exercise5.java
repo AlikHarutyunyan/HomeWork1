2a094d470af25011acf3a9d022f6d738b258bbff
Alik <alikharutyunyan00@gmail.com>
2022-11-16 13:20:39 +0200

    Refactored code to make it easier to read and work faster

--- src/Exercise5.java
+++ src/Exercise5.java
@@ -3,32 +3,29 @@ import java.util.Scanner;
 public class Exercise5 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
         System.out.println("Enter a number: ");
         int userInput = scanner.nextInt();
-
         int nextNumber;
         int previousNumber = 0;
         int currentNumber = 1;
 
-        if (userInput ==0) {
+        if (userInput == 0) {
             System.out.println("The number is fibonacci");
-        }
-
-        else {
+        }else {
             do  {
                 nextNumber = previousNumber + currentNumber;
                 previousNumber = currentNumber;
                 currentNumber = nextNumber;
 
-                if (nextNumber == userInput) {
-                    System.out.println("The number is fibonacci");
+                if (nextNumber >= userInput) {
+                    if(nextNumber == userInput) {
+                        System.out.println("The number is fibonacci");
+                    }else{
+                        System.out.println("The number is not fibonacci");
+                    }
                 }
 
-                else if (nextNumber > userInput){
-                    System.out.println("The number is not fibonacci");
-                }
-            } while (userInput > nextNumber);
+            }while (userInput > nextNumber);
         }
     }
 }
