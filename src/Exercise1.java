bfb0831536ecdf0a43a45b23bf3bc8e90632b161
Alik <alikharutyunyan00@gmail.com>
2022-11-18 19:52:53 +0200

    Small changes to the algorithm to make it more readable

--- src/Exercise1.java
+++ src/Exercise1.java
@@ -14,16 +14,17 @@ public class Exercise1 {
         thirdParameter = scanner.nextFloat();
 
         double quadEquationPart = Math.sqrt(secondParameter * secondParameter - 4 * firstParameter * thirdParameter);
+
         firstAnswer = (-secondParameter + quadEquationPart)/2*firstParameter;
         secondAnswer = (-secondParameter - quadEquationPart)/2*firstParameter;
 
-        if(firstAnswer >= 0 || firstAnswer < 0){
-            if(secondAnswer != firstAnswer){
-                System.out.println("Solved! There are 2 answers: x1 = "+ firstAnswer + " x2 = " + secondAnswer);
+        if(quadEquationPart >= 0){
+            if(firstAnswer != secondAnswer) {
+                System.out.println("Solved! There are 2 answers: x1 = " + firstAnswer + " x2 = " + secondAnswer);
             }else{
                 System.out.println("Solved! There is one answer: x1 = " + firstAnswer);
             }
-        }else{
+        }else {
             System.out.println("Unfortunately, there is no answer!");
         }
     }
