141efa6c6e513c04c8fbbd5b3b5acb763963a29a
Alik <alikharutyunyan00@gmail.com>
2022-11-19 02:59:51 +0200

    Optimized code, changes variable names.

--- src/Exercise3.java
+++ src/Exercise3.java
@@ -4,9 +4,6 @@ public class Exercise3 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int userInput;
-        int starCoordinate;
-        int starStart;
-        int starFinish;
 
         do {
             System.out.println("Write an odd number higher than 3: ");
@@ -14,33 +11,32 @@ public class Exercise3 {
             if(userInput % 2 == 0){
                 userInput = 0;
             }
-        } while (userInput <= 3);
+        }while (userInput <= 3);
 
-        starCoordinate = (userInput-1)/2;
-        starStart = starCoordinate;
-        starFinish = starCoordinate;
+        int middleStar = (userInput-1)/2;
+        int starLeft = middleStar;
+        int starRight = middleStar;
 
         for (int columnIndex = 0; columnIndex < userInput; columnIndex++) {
             for(int rowIndex=0; rowIndex<userInput; rowIndex++){
-                if (rowIndex >= starStart) {
+                if (rowIndex >= starLeft) {
                     System.out.print("*");
-                    if(rowIndex == starFinish){
+                    if(rowIndex == starRight){
                         break;
                     }
-                    continue;
+                }else{
+                    System.out.print(" ");
                 }
-                System.out.print(" ");
             }
             System.out.println();
 
-            if(columnIndex < starCoordinate){
-                starStart--;
-                starFinish++;
-                continue;
+            if(columnIndex < middleStar){
+                starLeft--;
+                starRight++;
+            }else {
+                starLeft++;
+                starRight--;
             }
-            starStart++;
-            starFinish--;
-
         }
     }
 }
